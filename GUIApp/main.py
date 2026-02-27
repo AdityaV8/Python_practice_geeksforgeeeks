28bbc1d16f3736ca5df9655e1d5e2e8697e12859
Aditya <adityaexists2002@gmail.com>
2026-02-27 12:25:01 +0530

    Add button widget to the TO-DO app and update window layout

--- GUIApp/main.py
+++ GUIApp/main.py
@@ -1,9 +1,13 @@
 import functions
 import FreeSimpleGUI as SG
 
+# individual widget creation
 label = SG.Text("Type in a todo")
 input_box = SG.InputText(tooltip="Enter todo")
+add_button = SG.Button("Add")
 
-window = SG.Window("The TO-DO App",layout=[[label,input_box]])
+#adding the widgets to the window
+window = SG.Window("The TO-DO App",layout=[[label,input_box,add_button]]) # the content should be python GUI widget type
 window.read()
+print("Hello")
 window.close()
